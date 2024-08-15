5239e3df2d4d46921ea153ee693185575dd777da
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-08-15 16:44:02 +0700

    tối ưu code

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,30 @@ import Log from "./components/log"
 import { WINNING_COMBINATIONS } from "./winning-combinations"
 import GameOver from "./components/GameOver"
 
-
+const PLAYERS ={
+  X: 'Player 1',
+  O: 'Player 2'
+};
 //----------------------------------------------------------------
-const initialGameBoard = [
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ]
 
 function deriveActivePlayer(gameTurns) {
-  let currentPlayer = 'X';
+  let currentPlayer = 'X'; // Giả sử người chơi hiện tại là 'X' mặc định.
 
+  // Nếu có bất kỳ lượt chơi nào trong trò chơi và lượt chơi gần đây nhất được thực hiện bởi 'X',
+  // thì lượt tiếp theo sẽ là của 'O'.
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
     currentPlayer = 'O';
   }
-  return currentPlayer;
+  return currentPlayer; // Trả về người chơi nên thực hiện lượt tiếp theo.
 }
 
-
-function App() {
-  const [gameTurns, setGameTurns] = useState([])
-  
-  const [players, setPlayers] = useState(
-    {
-      X: 'Player 1',
-      O: 'Player 2'
-    }
-  )
-
-  //const[activePlayer,setActivePlayer] = useState('X')
-  const activePlayer = deriveActivePlayer(gameTurns);
-  // khi restarts game thì tạo mới 
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player; // Update the board with the current player's symbol
-  }
+// check for a winner
+function deriveWinner(gameBoard,players){
   let winner = null;
   // Check for a winner
   for (const combination of WINNING_COMBINATIONS) {
@@ -58,9 +45,32 @@ function App() {
       winner = players[firstSquareSymbol];
     }
   }
+  return winner;
+}
+// khi restarts game thì tạo mới 
+function deriveGameBoard(gameTurns){
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player; // Update the board with the current player's symbol
+  }
+  return gameBoard;
+}
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([])
+  
+  const [players, setPlayers] = useState(PLAYERS)
+
+  //const[activePlayer,setActivePlayer] = useState('X')
+  const activePlayer = deriveActivePlayer(gameTurns);
+  // khi restarts game thì tạo mới 
+  const gameBoard = deriveGameBoard (gameTurns);
+  // Check for a winner
+  const winner = deriveWinner(gameBoard,players);
   // Check for a draw
   const hasDraw = gameTurns.length === 9 && !winner;
-
   // nút thao tác chơi
   function handlePlayerChange(rowIndex, colIndex) {
     //setActivePlayer((curActivePlayer) =>curActivePlayer === 'X'? 'O' : 'X')
@@ -88,12 +98,12 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName={'Player 1'}
+          <Player initialName={PLAYERS.X}
             symbol={'X'}
             isActive={activePlayer === 'X'}
             onChangeName={handlePlayerNameChange}
           />
-          <Player initialName={'Player 2'}
+          <Player initialName={PLAYERS.O}
             symbol={'O'}
             isActive={activePlayer === 'O'}
             onChangeName={handlePlayerNameChange}
