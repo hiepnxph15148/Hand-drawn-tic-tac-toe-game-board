9c9cd420809b053549a4df3327af7bb3319b4076
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-08-15 13:43:28 +0700

    gameboard tic-tac

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,37 +1,37 @@
 import React, { useState } from 'react'
 
-const initialGameBoard =[
-    [null,null,null],
-    [null,null,null],
-    [null,null,null],
-]
 
- const GameBoard = ({ onSelectSquare, activePlayerSymbol}) => {
-    const [gameBoard,setGameBoard] = useState(initialGameBoard)
+const GameBoard = ({ onSelectSquare, board }) => {
 
-    function handleSelectSquare(rowIndex,colIndex){
-        setGameBoard((prevGameBoard) =>{
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol; // Update the board with the current player's symbol
-            return updatedBoard;
-        })
-        onSelectSquare();
-    }
-  return (
-    <ol id='game-board'>
-        {gameBoard.map((row,rowIndex) => 
-            <li key={rowIndex}>
-                <ol>
-                    {row.map((symbolPlayer,colIndex) => 
-                    <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{symbolPlayer}</button>
-                    </li>
-                    )}
-                </ol>
-            </li>
-        )}
-    </ol>
-  )
+
+
+    // const [gameBoard,setGameBoard] = useState(initialGameBoard)   
+    // function handleSelectSquare(rowIndex,colIndex){
+    //     setGameBoard((prevGameBoard) =>{
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol; // Update the board with the current player's symbol
+    //         return updatedBoard;
+    //     })
+    //     onSelectSquare();
+    // }
+    return (
+        <ol id='game-board'>
+            {board.map((row, rowIndex) =>
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) =>
+                            <li key={colIndex}>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                                    disabled={playerSymbol !== null}>
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        )}
+                    </ol>
+                </li>
+            )}
+        </ol>
+    )
 }
 export default GameBoard;
 
---
+++ src/components/GameOver.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const GameOver = ({ winner }) => {
+    return (
+        <div id='game-over'>
+            <h2>GameOver !</h2>
+            {winner &&<p>{winner} won!</p>}
+            {!winner &&<p>Is&apos;s a draw!</p>}
+            <p>
+                <button>Rematch!</button>
+            </p>
+        </div>
+    )
+}
+
+export default GameOver
