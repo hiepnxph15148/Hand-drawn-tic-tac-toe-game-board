02e9e316d82b8094bd05ce4b4c1676a3503bd5fa
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-08-15 16:51:29 +0700

    sửa import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
-import Log from "./components/log"
+import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations"
 import GameOver from "./components/GameOver"
 
